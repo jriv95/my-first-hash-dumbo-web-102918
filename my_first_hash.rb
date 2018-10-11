558f6b59adb73cd4e1adceee1de78a9ec5c0439b
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-11 19:13:26 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,9 +11,9 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 the_manifest = {
-  "whale_bone_corsets" => 5,
-  "porcelain_vases" => 2,
-  "oil_paintings" => 3
+  "whale bone corsets" => 5,
+  "porcelain vases" => 2,
+  "oil paintings" => 3
 }
 
 end
@@ -26,7 +26,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-the_manifest["oil_paintings"]
+the_manifest["oil paintings"]
 
 end
 
@@ -46,7 +46,7 @@ def adding
 
   # return the shipping_manifest hash below
 
-the_manifest["muskets" => 2, "gun_powder" => 4]
+the_manifest["muskets" => 2, "gun powder" => 4]
 
 the_manifest
 end
