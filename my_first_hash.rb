cf30220cd5b224e36eb0380194bc3cc25e82d5ea
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-11 19:17:00 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -47,7 +47,7 @@ def adding
   # return the shipping_manifest hash below
 
 shipping_manifest["muskets"] = 2
-shipping_manifest ["gun powder"] = 4
+shipping_manifest["gun powder"] = 4
 
 shipping_manifest
 end
